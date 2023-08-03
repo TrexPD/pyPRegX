121fb97659411b74ceb117dffb585b5b8cadd000
TrexPD <leynad157@gmail.com>
2023-08-03 16:01:04 -0300

    initial commit with all files!

---
+++ pypregx/pregx.py
@@ -0,0 +1,15 @@
+
+
+class ParserRegex():
+    def __init__(self, file_path: str):
+        with open(file_path, "rt", encoding="utf-8") as file:
+            self.content = file.read()
+
+
+
+
+
+
+
+if __name__ in "__main__":
+    parser = ParserRegex(r"C:\Users\leyna\Desktop\meupdf.html")
